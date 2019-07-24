2970dccc7c0099b2714a7fa8a06526588c772c7a
jimfede <jimfede@gmail.com>
2019-07-24 16:04:47 -0300

    cosas

--- ActualizadorTrazpro/Form1.Designer.cs
+++ ActualizadorTrazpro/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Actualizar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(133, 104);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(447, 248);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- ActualizadorTrazpro/Form1.cs
+++ ActualizadorTrazpro/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,73 @@ namespace ActualizadorTrazpro
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            progressBar1.Value = 0;
+            CopyFolderContents("\\\\10.26.32.13\\ActualizacionTrazPro\\EjecutablesRotulos\\Desosado", "c:\\TrazproTest");
+            /*CopyFolderContents("\\\\10.26.32.13\\ActualizacionTrazPro\\EjecutablesRotulos\\Desosado", "c:\\TrazproTest");
+            CopyFolderContents("\\\\10.26.32.13\\ActualizacionTrazPro\\EjecutablesTrazPro", "c:\\TrazproTest");*/
+
+        }
+
+        public void CopyFolderContents(string sourceFolder, string destinationFolder)
+        {
+            CopyFolderContents(sourceFolder, destinationFolder, "*.*", false, false);
+        }
+
+        public void CopyFolderContents(string sourceFolder, string destinationFolder, string mask)
+        {
+            CopyFolderContents(sourceFolder, destinationFolder, mask, false, false);
+        }
+
+        public void CopyFolderContents(string sourceFolder, string destinationFolder, string mask, Boolean createFolders, Boolean recurseFolders)
+        {
+            try
+            {
+
+                if (!sourceFolder.EndsWith(@"\")) { sourceFolder += @"\"; }
+                if (!destinationFolder.EndsWith(@"\")) { destinationFolder += @"\"; }
+
+                var exDir = sourceFolder;
+                var dir = new DirectoryInfo(exDir);
+                SearchOption so = (recurseFolders ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+
+                foreach (string sourceFile in Directory.GetFiles(dir.ToString(), mask, so))
+                {
+                    FileInfo srcFile = new FileInfo(sourceFile);
+                    string srcFileName = srcFile.Name;
+
+                    // Create a destination that matches the source structure
+                    FileInfo destFile = new FileInfo(destinationFolder + srcFile.FullName.Replace(sourceFolder, ""));
+
+                    if (!Directory.Exists(destFile.DirectoryName) && createFolders)
+                    {
+                        Directory.CreateDirectory(destFile.DirectoryName);
+                    }
+
+                    if (srcFile.LastWriteTime > destFile.LastWriteTime || !destFile.Exists)
+                    {
+                        File.Copy(srcFile.FullName, destFile.FullName, true);
+
+                        progressBar1.PerformStep();
+                    }
+                                        
+                }
+
+                label1.Text = "Actualización completada";
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message + Environment.NewLine + Environment.NewLine + ex.StackTrace);
+            }
+        }
+
     }
 }
